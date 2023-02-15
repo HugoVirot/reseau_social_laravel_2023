e057bfb6422b570af55341aa2c6e22e43f5f612f
HugoVirot <hugo.virot@gmail.com>
2023-02-15 09:59:41 +0100

    migrations, modèles, seeders et authentification ok

---
+++ app/Models/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pseudo', 'email', 'password', 'image'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // nom au pluriel car un user peut poster plusieurs quacks
+    // cardinalité 0,n
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    // nom au pluriel car un user peut poster plusieurs commentaires
+    // cardinalité 0,n
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // nom de la fonction au singulier car 1 seul rôle en relation
+    // cardinalité 1,1
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function isAdmin()
+    {
+        //return $this->role == "admin";
+        return $this->role_id == 2;
+    }
+}
